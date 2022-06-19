7db21bb290e4824fb1dcf1e9fa6b1509e93b3d08
Andysimmonsisme <andysimmons78@gmail.com>
2022-06-19 15:25:46 -0400

    add highlight to app module/component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from '../category/category.component';
 import { CategoryDetailsComponent } from '../category-details/category-details.component';
+import { HighlightComponent } from '../highlight/highlight.component';
 
 import { DataService } from '../data/data.service';
 
@@ -19,6 +20,7 @@ import { IsNumberPipe } from '../category-details/is-number.pipe';
     AppComponent,
     CategoryComponent,
     CategoryDetailsComponent,
+    HighlightComponent,
     RenderDetailsDirective,
     IsNumberPipe,
   ],
